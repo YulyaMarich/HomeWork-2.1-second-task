a04971e883c4b05d4d50ba4deb0f556a9695e328
Yulia Marych <yulyamarich@icloud.com>
2021-08-30 14:59:11 +0300

    Added view dynamics.

--- HomeWork2.1 second task/ViewController.swift
+++ HomeWork2.1 second task/ViewController.swift
@@ -8,24 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var redView: UIView!
-    
     @IBOutlet weak var yellowView: UIView!
-    
     @IBOutlet weak var greenView: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         redView.alpha = 0.3
         yellowView.alpha = 0.3
         greenView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
+    }
+    override func viewWillLayoutSubviews() {
         redView.layer.cornerRadius = redView.frame.height / 2
         yellowView.layer.cornerRadius = yellowView.frame.height / 2
         greenView.layer.cornerRadius = greenView.frame.height / 2
-        startButton.layer.cornerRadius = 10
     }
-
+    
     @IBAction func startButtonPressed() {
         if redView.alpha < 1 && yellowView.alpha < 1 && greenView.alpha < 1 {
             redView.alpha = 1
@@ -37,9 +40,9 @@ class ViewController: UIViewController {
             greenView.alpha = 1
             yellowView.alpha = 0.3
         } else if redView.alpha < 1 && yellowView.alpha < 1 && greenView.alpha == 1 {
-        redView.alpha = 1
-        greenView.alpha = 0.3
+            redView.alpha = 1
+            greenView.alpha = 0.3
         }
     }
 }
-    
+
